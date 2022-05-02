8e47ece91e202f9c503539d6680f9f30f648485d
sampet93 <47121537+sampet93@users.noreply.github.com>
2022-05-02 18:50:20 +0300

    UI fixing

--- src/components/WeatherInfoBoxBig/WeatherInfoBoxBig.js
+++ src/components/WeatherInfoBoxBig/WeatherInfoBoxBig.js
@@ -5,25 +5,31 @@ import "./WeatherInfoBoxBig.css";
 const WeatherInfoBoxBig = () => {
   return (
     <Grid container justifyContent="center" className="parent-container">
-      <Grid item xs={6}>
-        <div className="flex-container top-left-container">
-          <p>Testi</p>
-        </div>
+      <Grid container xs={6} flexDirection="column">
+        <Grid item className="top-left-container">
+          <h2>Helsinki</h2>
+          <p>Scattered clouds</p>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <div className="flex-container top-right-container">
-          <p>Testi</p>
-        </div>
+      <Grid container xs={6}>
+        <Grid item justifyContent="center">
+          <h2>Testi</h2>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <div className="flex-container bottom-left-container">
-          <p>Testi</p>
-        </div>
+      <Grid container xs={6}>
+        <Grid item className="bottom-left-container">
+          <h3>May 2nd</h3>
+          <p>03:00</p>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <div className="flex-container bottom-right-container">
-          <p>Testi</p>
-        </div>
+      <Grid container xs={6} justifyContent="right">
+        <Grid item justifyContent="right">
+          <div className="bottom-right-container">
+            <p>Wind: 3 m/s</p>
+            <p>Humidity: 03:00</p>
+            <p>03:00</p>
+          </div>
+        </Grid>
       </Grid>
     </Grid>
   );
