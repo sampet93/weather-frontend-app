fa5f722187b900434081d92cd6830db66de28581
sampet93 <47121537+sampet93@users.noreply.github.com>
2022-05-22 14:06:53 +0300

    added cities

--- src/cities.js
+++ src/cities.js
@@ -4,6 +4,8 @@ const cities = [
   { id: 655195, name: "Jyväskylä" },
   { id: 650225, name: "Kuopio" },
   { id: 660129, name: "Espoo" },
+  { id: 658225, name: "Helsinki" },
+  { id: 633679, name: "Turku" },
 ];
 
 export default cities;
